187a97f57831e0f7348f6699cec04f6215539a5b
Abhishek madoliya <abhishekmadoliya@gmail.com>
2025-03-07 13:23:12 +0530

    js basic : let var const

---
+++ javascript/js1.js
@@ -0,0 +1,50 @@
+
+// js makes ui dynamic
+// js makes ui interactive
+// js makes ui responsive
+// js makes ui user friendly
+// js makes ui fast
+// js makes ui secure
+// js makes ui dynamic
+
+
+// var vs let vs const
+// var -> function scope means var is accessible within function
+// let -> block scope means let is accessible within block
+// const -> block scope means const is accessible within block
+
+
+// js is a synchous language
+// js is a single threaded language
+
+//js is loosely typed language
+//js is dynamically typed language
+
+
+//printing method
+// console.log("hello world");
+//alert("hello world");
+// prompt("hello world");
+// confirm("hello world");
+//document.write("hello world");
+
+// sync vs async
+// sync -> line by line execution
+// async -> line by line execution not guaranteed
+
+
+
+// primitive data types
+// string
+// number
+// boolean
+// null
+// undefined
+// symbol
+// bigint
+
+// non primitive data types
+// object
+// array
+// function
+
